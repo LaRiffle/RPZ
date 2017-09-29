e26183c87ee507addc9e9082c16c7f88c2497333
Theo <theo.ryffel@hotmail.fr>
2017-09-29 09:23:00 +0100

    Add LFR first display

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new RPZ\DiscussionBundle\RPZDiscussionBundle(),
             new RPZ\UserBundle\RPZUserBundle(),
+            new LFR\StoreBundle\LFRStoreBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/LFR/StoreBundle/LFRStoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LFR\StoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LFRStoreBundle extends Bundle
+{
+}
