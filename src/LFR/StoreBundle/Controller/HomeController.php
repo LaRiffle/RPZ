6ebd8363d84276dd3873c033118ae36fe693ce02
Theo <theo.ryffel@hotmail.fr>
2017-09-30 21:01:20 +0100

    Fix issues and add effects and components - Add a carousel with a galery attached and zoom effect - Add effect on double img in history - Add auto scroll effects on frist page - create a home page != first page - etc

--- src/LFR/StoreBundle/Controller/HomeController.php
+++ src/LFR/StoreBundle/Controller/HomeController.php
@@ -4,10 +4,18 @@ namespace LFR\StoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class HomeController extends Controller
 {
-    public function indexAction()
+    public function animationAction()
     {
-        return $this->render('LFRStoreBundle:Default:index.html.twig');
+        return $this->render('LFRStoreBundle:Home:animation.html.twig');
+    }
+    public function homepageAction()
+    {
+        return $this->render('LFRStoreBundle:Home:home_page.html.twig');
+    }
+    public function homeAction()
+    {
+        return $this->render('LFRStoreBundle:Home:home.html.twig');
     }
 }
